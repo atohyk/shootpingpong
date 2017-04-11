44814ca1b96285403f50dc126eec8e4ffeaf34f2
atohyk <toh.yew.kwan@gmail.com>
2017-04-11 23:51:18 +0800

    Made the arduino sketch simpler. Sending Pxxx will write xxx directly to the pitch servo pin, writing Yxxx will write xxx directly to the yaw servo pin. The units are in angles, from 0-180, and the code makes direct use of the arduino servo library. Sending S will result in the shoot pin being brought high for a short period of time.

--- shootpingpong.ino
+++ shootpingpong.ino
@@ -1,81 +1,50 @@
 #include <Servo.h>
-#define RX_BUFFER_SIZE 10
-#define MSG_SHOOT_ID 1
-#define MSG_AIM_ID 2
+#include <string.h>
 
-const char shootPin = 9;
+const char shootPin = 3;
 const char yawPin = 10;
-const char pitchPin = 3;
+const char pitchPin = 9;
 
 Servo pitchServo;
 Servo yawServo;
 
-char rxBuffer[RX_BUFFER_SIZE];
+unsigned char pitchAngle, yawAngle;
 
-typedef struct {
-  char msgID,
-  unsigned char msgLength;
-  unsigned char pitchAngle, yawAngle;
-}SerialMsg_t;
-
-SerialMsg_t serialMsg;
-
-enum {
-  WAITING_FOR_MSG_ID,
-  WAITING_FOR_PITCH,
-  WAITING_FOR_YAW,
-} serialState;
-
-void shootPingPong(){
-  //set D pin to high 
-  digitalWrite(shootPin, HIGH);
-  delay(200);
-  digitalWrite(shootPin, LOW);
-}
-void updateSerial(char incomingByte){
-  switch (serialState){
-    case WAITING_FOR_MSG_ID:
-     if(incomingByte == 'P'){
-      serialState = WAITING_FOR_PITCH;
-     }
-     else if(incomingByte == 'Y'){
-      serialState = WAITING_FOR_YAW;
-     }
-     else if(incomingByte == 'S'){
-      shootPingPong();
-     }
-     else{
-      //reset
-      memset(&serialMsg, 0, sizeof(serialMsg));
-     }
-     break;
-    case WAITING_FOR_PITCH:
-      serialMsg.pitchAngle = Serial.parseInt();
-      pitchServo.write(serialMsg.pitchAngle);
-      serialState = WAITING_FOR_MSG_ID;
-      break;
-    case WAITING_FOR_YAW:
-      serialMsg.yawAngle = Serial.parseInt();
-      yawServo.write(serialMsg.yawAngle);
-      serialState = WAITING_FOR_MSG_ID;
-      break;
-     }
+void updateSerial(char incomingByte) {
+  if (incomingByte == 'P') {
+    pitchAngle = Serial.parseInt() & 0xFF;
+    pitchServo.write(pitchAngle);
+    Serial.print("P");
+    Serial.println(pitchAngle);
+  }
+  else if (incomingByte == 'Y') {
+    yawAngle = Serial.parseInt() & 0xFF;
+    yawServo.write(yawAngle);
+    Serial.print("Y");
+    Serial.println(yawAngle);
+  }
+  else if (incomingByte == 'S') {
+    Serial.println("Shoot");
+    digitalWrite(shootPin, HIGH);
+    delay(200);
+    digitalWrite(shootPin, LOW);
+  }
+  return;
 }
 
 void setup() {
   // put your setup code here, to run once:
- Serial.begin(9600);
- delay(10);
- pinMode(shootPin, OUTPUT);
- pitchServo.attach(pitchPin);
- yawServo.attach(yawPin);
- memset(&rxBuffer[0], 0, sizeof(rxBuffer));
- memset(&serialMsg, 0, sizeof(serialMsg));
+  Serial.begin(115200);
+  delay(10);
+  pinMode(shootPin, OUTPUT);
+  pitchServo.attach(pitchPin);
+  yawServo.attach(yawPin);
+  Serial.println("Start");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(Serial.available()){
+  if (Serial.available()) {
     updateSerial(Serial.read());
   }
 }
