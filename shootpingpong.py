636899bb4862912fc18c8103d7bd9452db424faf
atohyk <toh.yew.kwan@gmail.com>
2017-04-12 12:10:09 +0800

    Added a second screen, tested background subtraction with small drone.

--- shootpingpong.py
+++ shootpingpong.py
@@ -15,7 +15,7 @@ lastPrintTime = 0
 ser = serial.Serial()
 ser.baudrate = 115200
 ser.port= 'COM12'
-ser.open()
+#ser.open()
 
 if not ser.is_open:
     print('Unable to open Serial Port')
@@ -41,27 +41,29 @@ while(True):
         cX = int(m["m10"] / m["m00"])
         cY = int(m["m01"] / m["m00"])
         cv2.circle(closing, (cX, cY), 50, (255,255,255), 10)
-        cv2.imshow('frame',closing)
+        cv2.circle(frame, (cX, cY), 50, (255,0,0), 10)
+        cv2.imshow('closing',closing)
     else:
         cX = -1
         cY = -1
-
+        
+    cv2.imshow('frame', frame)
     #CONVERT cX and cY into pitchAngle and yawAngle
-
-    #write angles to string
-    if(millis() - lastTime > 50) #update rate of 20hz
-    {
-        lastTime = millis()
-        pitchAngleString = 'P'+str(pitchAngle)+'\n'
-        yawAngleString = 'Y'+str(yawAngle)+'\n'
-        shootString = 'S'+'\n'
-
-        #send string through serial
-        ser.write(pitchAngleString.encode('utf-8'))
-        ser.write(yawAngleString.encode('utf-8'))
-        ser.write(shootString.encode('utf-8'))
-    }
     
+    #write angles to string
+    if(millis() - lastPrintTime > 50) : #update rate of 20hz
+        if ser.is_open:
+            lastTime = millis()
+            pitchAngleString = 'P'+str(pitchAngle)+'\n'
+            yawAngleString = 'Y'+str(yawAngle)+'\n'
+            shootString = 'S'+'\n'
+
+            #send string through serial
+            ser.write(pitchAngleString.encode('utf-8'))
+            ser.write(yawAngleString.encode('utf-8'))
+            ser.write(shootString.encode('utf-8'))
+    
+       
     #quit if user presses q
     if cv2.waitKey(1) & 0xFF == ord('q'):
         print(fgmask)
